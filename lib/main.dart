ac9e073f308021ba1f17ff524abda68a16302c85
Eren Akel <eren.akel.46@gmail.com>
2021-12-16 19:18:29 +0300

    Login page created but it will be changed

--- lib/main.dart
+++ lib/main.dart
@@ -130,7 +130,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                   curve: Curves.linear,
                   child: Image.network(
-                    'https://ouch-cdn2.icons8.com/As6ct-Fovab32SIyMatjsqIaIjM9Jg1PblII8YAtBtQ/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNTg4/LzNmMDU5Mzc0LTky/OTQtNDk5MC1hZGY2/LTA2YTkyMDZhNWZl/NC5zdmc.png',
+                    'https://freepngimg.com/thumb/minecraft/5-2-minecraft-enderman-png.png',
                     height: 400,
                   ),
                 ),
@@ -145,7 +145,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   duration: const Duration(seconds: 1),
                   curve: Curves.linear,
                   child: Image.network(
-                    'https://ouch-cdn2.icons8.com/vSx9H3yP2D4DgVoaFPbE4HVf6M4Phd-8uRjBZBnl83g/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNC84/MzcwMTY5OS1kYmU1/LTQ1ZmEtYmQ1Ny04/NTFmNTNjMTlkNTcu/c3Zn.png',
+                    'https://pngimg.com/uploads/minecraft/minecraft_PNG94.png',
                     height: 400,
                   ),
                 ),
@@ -160,7 +160,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   duration: const Duration(seconds: 1),
                   curve: Curves.linear,
                   child: Image.network(
-                    'https://ouch-cdn2.icons8.com/fv7W4YUUpGVcNhmKcDGZp6pF1-IDEyCjSjtBB8-Kp_0/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvMTUv/ZjUzYTU4NDAtNjBl/Yy00ZWRhLWE1YWIt/ZGM1MWJmYjBiYjI2/LnN2Zw.png',
+                    'https://pngimg.com/uploads/minecraft/minecraft_PNG47.png',
                     height: 400,
                   ),
                 ),
@@ -175,7 +175,22 @@ class _LoginScreenState extends State<LoginScreen> {
                   duration: const Duration(seconds: 1),
                   curve: Curves.linear,
                   child: Image.network(
-                    'https://ouch-cdn2.icons8.com/AVdOMf5ui4B7JJrNzYULVwT1z8NlGmlRYZTtg1F6z9E/rs:fit:784:767/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvOTY5/L2NlMTY1MWM5LTRl/ZjUtNGRmZi05MjQ3/LWYzNGQ1MzhiOTQ0/Mi5zdmc.png',
+                    'https://pngimg.com/uploads/minecraft/minecraft_PNG71.png',
+                    height: 400,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: AnimatedOpacity(
+                  opacity: activeIndex == 3 ? 1 : 0,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.linear,
+                  child: Image.network(
+                    'https://pngimg.com/uploads/minecraft/minecraft_PNG48.png',
                     height: 400,
                   ),
                 ),
