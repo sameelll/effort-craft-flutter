fa44038b5fee915deca20f049efbd256c278df47
Eren Akel <eren.akel.46@gmail.com>
2021-12-20 14:09:00 +0300

    Fetching user data with uid handled

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,48 +1,67 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  final User? user;
+  const ProfileScreen({Key? key, this.user}) : super(key: key);
 
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  final Stream<QuerySnapshot> users =
-      FirebaseFirestore.instance.collection('users').snapshots();
+  User? user;
+  Future<void> getUserData() async {
+    var userData = FirebaseAuth.instance.currentUser;
+    setState(() {
+      user = userData;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUserData();
+  }
+
   @override
   Widget build(BuildContext context) {
     CollectionReference users = FirebaseFirestore.instance.collection('users');
     return Scaffold(
-      extendBody: true,
-      bottomNavigationBar: const BottomNavBarRaisedInsetFb1(),
-      backgroundColor: const Color(0xFF393E46),
-      body: Container(
-          height: 115,
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-          child: StreamBuilder<QuerySnapshot>(
-            stream: users.snapshots(),
-            builder:
-                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.hasError) {
-                return const Text('Something went wrong.');
-              }
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Text('Loading...');
-              }
+        extendBody: true,
+        bottomNavigationBar: const BottomNavBarRaisedInsetFb1(),
+        backgroundColor: const Color(0xFF393E46),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 70),
+          child: Container(
+            height: 115,
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+            child: FutureBuilder<DocumentSnapshot>(
+              future: users.doc(user?.uid).get(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<DocumentSnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return const Text("Something went wrong");
+                }
 
-              final data = snapshot.requireData;
+                if (snapshot.hasData && !snapshot.data!.exists) {
+                  return const Text("Document does not exist");
+                }
 
-              return ListView.builder(
-                  itemCount: data.size,
-                  itemBuilder: (context, index) {
-                    return Text('Welcome back ${data.docs[index]['name']} !');
-                  });
-            },
-          )),
-    );
+                if (snapshot.connectionState == ConnectionState.done) {
+                  Map<String, dynamic> data =
+                      snapshot.data!.data() as Map<String, dynamic>;
+                  return Text(
+                      "Full Name: ${data['name']}, Email: ${data['email']}");
+                }
+
+                return const Text("loading");
+              },
+            ),
+          ),
+        ));
   }
 }
 
